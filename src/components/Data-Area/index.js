d12e0209d3250c1b6946e957e82dfc970e0a92b1
Cody Chen <codychen1@gmail.com>
2021-02-21 11:57:11 -0700

    added state function to Dataarea index.js

--- src/components/Data-Area/index.js
+++ src/components/Data-Area/index.js
@@ -4,3 +4,29 @@ import Nav from "../Nav";
 import API from "../../utils/API";
 import "./Data-Area.css";
 import DataAreaContext from "../../utils/DataAreaContext";
+
+const DataArea = () => {
+  const [developerState, setDeveloperState] = useState({
+    users: [],
+    order: "ascend",
+    filteredUsers: [],
+    headings: [
+      { name: "Image", width: "10%" },
+      { name: "Name", width: "10%" },
+      { name: "Phone", width: "20%" },
+      { name: "Email", width: "20%" },
+      { name: "DOB", width: "10%" },
+    ],
+  });
+  const handleSort = (heading) => {
+    if (developerState.order === "descend") {
+      setDeveloperState({
+        order: "ascend",
+      });
+    } else {
+      setDeveloperState({
+        order: "descend",
+      });
+    }
+  };
+};
