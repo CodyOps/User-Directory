9ae82abd6906dd4903aee40d878530a35e216ed6
Cody Chen <codychen1@gmail.com>
2021-02-21 12:29:38 -0700

    added to datatable index.js

--- src/components/Data-Table/index.js
+++ src/components/Data-Table/index.js
@@ -3,3 +3,9 @@ import React, { useContext } from "react";
 import DataBody from "../Data-Body";
 import "./Data-Table.css";
 import DataAreaContext from "../../utils/DataAreaContext";
+
+const DataTable = () => {
+  const context = useContext(DataAreaContext);
+};
+
+export default DataTable;
