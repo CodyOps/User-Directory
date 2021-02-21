b9c36675fa801d03322d14b0e9c7eec83df9214f
Cody Chen <codychen1@gmail.com>
2021-02-21 12:45:24 -0700

    added h2 to header for more info to user

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,8 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <h1>Employee Directory</h1>
+      <h1>Employee Directory Portal</h1>
+      <h3>Search for any Employee!</h3>
       <p>
         Use the search box to narrow your results, or click on each heading to
         filter by section.
