69572bfc746725c50439c47e6060831806e547bd
Cody Chen <codychen1@gmail.com>
2021-02-21 11:25:22 -0700

    created component for main index.js

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DataArea from "../Data-Area/index.js";
+import "./Main.css";
+
+function Main() {
+  return (
+    <>
+      <DataArea />
+    </>
+  );
+}
+
+export default Main;
