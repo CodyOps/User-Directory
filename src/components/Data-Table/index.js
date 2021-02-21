cd529088d8a3eafc7969f2fdbe7867f0fe66e33c
Cody Chen <codychen1@gmail.com>
2021-02-21 12:28:27 -0700

    imported usecontext and files to datatable index.js

---
+++ src/components/Data-Table/index.js
@@ -0,0 +1,5 @@
+//import useContext, DataBody, DataTable and DataAreaContext
+import React, { useContext } from "react";
+import DataBody from "../Data-Body";
+import "./Data-Table.css";
+import DataAreaContext from "../../utils/DataAreaContext";
