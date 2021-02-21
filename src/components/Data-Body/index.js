00a4836e0ec92c4e06e2ffe1ea4df11469285c55
Cody Chen <codychen1@gmail.com>
2021-02-21 12:13:15 -0700

    added to databody index.js

---
+++ src/components/Data-Body/index.js
@@ -0,0 +1,4 @@
+//Import in useContext, Data-body css and DataAreaContext
+import React, { useContext } from "react";
+import "./Data-Body.css";
+import DataAreaContext from "../../utils/DataAreaContext";
