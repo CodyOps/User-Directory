54a883df813fbf9b084e257087a073d9faeba7fc
Cody Chen <codychen1@gmail.com>
2021-02-21 11:40:55 -0700

    added imports in to data-area index.js

---
+++ src/components/Data-Area/index.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from "react";
+import DataTable from "../Data-Table";
+import Nav from "../Nav";
+import API from "../../utils/API";
+import "./Data-Area.css";
+import DataAreaContext from "../../utils/DataAreaContext";
