c81287fc488d0630d8d308521a160fcb0334293c
Cody Chen <codychen1@gmail.com>
2021-02-21 11:17:56 -0700

    added header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>
+        Use the search box to narrow your results, or click on each heading to
+        filter by section.
+      </p>
+    </div>
+  );
+}
+
+export default Header;
